f9740966721ea22fba0d4f38c25c7019814a33a6
Aurélien Questel <aurelien.questel@planisense.com>
2025-01-13 14:18:57 +0100

    [conan]removing solver level in build path

--- conanfile.py
+++ conanfile.py
@@ -79,8 +79,7 @@ class libreflectConan(ConanFile):
         compiler_version = str(self.settings.compiler.version)
         arch = str(self.settings.arch)
         build_type = str(self.settings.build_type)
-        solvers = "_"
-        return os.path.join("builds", system, compiler, compiler_version, arch, build_type, solvers)
+        return os.path.join("builds", system, compiler, compiler_version, arch, build_type)
 
     def layout(self):
         build_dir = self._get_build_dir()
