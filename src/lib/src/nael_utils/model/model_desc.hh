5061c3aae1d27688c610967fa6cebf9d31b39e26
Aurélien Questel <aurelien.questel@planisense.com>
2025-02-04 15:55:01 +0100

    ++N_SEC_IN_HOUR

--- src/lib/src/nael_utils/model/model_desc.hh
+++ src/lib/src/nael_utils/model/model_desc.hh
@@ -13,6 +13,7 @@ namespace bpt = boost::posix_time;
 namespace bg = boost::gregorian;
 
 #define N_SEC_IN_DAY 86400
+#define N_SEC_IN_HOUR 3600
 
 /**
  * @brief Create an operator< based on a list of attributes.<br/>
