f0f054e52069e90c8de715ea0eb0fff894591ca7
Aurélien Questel <aurelien.questel@planisense.com>
2025-07-11 18:44:45 +0200

    ++MAKE_NON_COPYABLE_ATT for non copyable Objects

--- src/lib/src/nael_utils/model/model_desc.hh
+++ src/lib/src/nael_utils/model/model_desc.hh
@@ -153,6 +153,12 @@ MAKE_ATT_GETTER_VARIABLE(,0,att_desc)\
 MAKE_ATT_GETTER_VARIABLE(,1,att_desc)\
 MAKE_ATT_SETTER_VARIABLE(,,att_desc)
 
+#define MAKE_NON_COPYABLE_ATT( att_desc ) \
+private: MAKE_CLASS_ATT_DECL_VARIABLE(,,att_desc) \
+public: \
+MAKE_ATT_GETTER_VARIABLE(,0,att_desc)\
+MAKE_ATT_GETTER_VARIABLE(,1,att_desc)
+
 /**
  * @brief Generate the declaration and every get for basic non modifiable attribute
  * @code{cpp}
