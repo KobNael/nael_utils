a3351edc926b13219b2044a4fbfc48c2a91ae20e
Aurélien Questel <aurelien.questel@planisense.com>
2025-08-28 14:32:45 +0200

    ++operator< on AbstractIdx

--- src/lib/src/nael_utils/model/AbstractIdx.hh
+++ src/lib/src/nael_utils/model/AbstractIdx.hh
@@ -16,7 +16,7 @@
  *      size_t _val;
  *      explicit ObjIdx(): _val(std::numeric_limits<size_t>::max()) {}
  *      explicit ObjIdx(const size_t &val_p): _val(val_p) {}
- *      bool isUnsetted() const {return _val == class_name()._val;
+ *      bool is_unsetted() const {return _val == class_name()._val;
  * }
  * @endcode
  */
@@ -27,10 +27,14 @@
         size_t _val;                                                           \
         explicit class_name() : _val(std::numeric_limits<size_t>::max()) {}    \
         explicit class_name(const size_t &val_p) : _val(val_p) {}              \
-        bool isUnsetted() const { return _val == class_name()._val; }          \
+        bool is_unsetted() const { return _val == class_name()._val; }          \
         std::ostream &stream(std::ostream &os) const override                  \
         {                                                                      \
             return os << BOOST_PP_STRINGIZE(class_name) << "(" << _val << ")"; \
         }                                                                      \
         bool operator==(class_name const &) const = default;                   \
+        bool operator<(class_name const &other) const                          \
+        {                                                                      \
+            return _val < other._val;                                          \
+        }                                                                      \
     };
