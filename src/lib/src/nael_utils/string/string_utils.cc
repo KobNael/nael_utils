8962ca10ef1866d3bcb60884e23a785c369dbb90
Aurélien Questel <aurelien.questel@planisense.com>
2026-02-05 21:23:19 +0100

    enhance get_val_from_str

--- src/lib/src/nael_utils/string/string_utils.cc
+++ src/lib/src/nael_utils/string/string_utils.cc
@@ -3,6 +3,8 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/posix_time/posix_time_duration.hpp>
 #include <boost/date_time/gregorian/gregorian_types.hpp>
+
+#include <format>
 #include <string_view>
 #include <ranges>
 
@@ -52,15 +54,73 @@ namespace str
     }
 
     template <>
+    bool get_val_from_str<bool>(const std::string &str_val)
+    {
+        static auto true_values = {"1", "true", "True", "TRUE"};
+        static auto false_values = {"0", "false", "False", "FALSE"};
+        if(std::ranges::find(true_values, str_val) != true_values.end())
+            return true;
+        else if(std::ranges::find(false_values, str_val) != false_values.end())
+            return false;
+        else
+            throw bad_lexical_cast("Cannot parse value : " + str_val);
+    }
+
+    template <>
     bg::date get_val_from_str<bg::date>(const std::string &str_val)
     {
-        return bg::date_from_iso_string(str_val);
+        try
+        {
+            return bg::date_from_iso_string(str_val);
+        }
+        catch(boost::bad_lexical_cast const&e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
+        catch(std::out_of_range const &e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
     }
 
     template <>
     bpt::ptime get_val_from_str<bpt::ptime>(const std::string &str_val)
     {
-        return bpt::from_iso_extended_string(str_val);
+        try
+        {
+            return bpt::from_iso_extended_string(str_val);
+        }
+        catch(boost::bad_lexical_cast const&e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
+        catch(std::out_of_range const &e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
+    }
+
+    template <>
+    bpt::time_duration get_val_from_str<bpt::time_duration>(const std::string &str_val)
+    {
+        try
+        {
+            return bpt::duration_from_string(str_val);
+        }
+        catch(boost::bad_lexical_cast const&e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
+        catch(std::out_of_range const &e)
+        {
+            throw bad_lexical_cast(
+                std::format("Cannot parse value {}: {} ", str_val, std::string(e.what())));
+        }
     }
 
 } // namespace str
