ad6360227927361e598ec4857248489a5107f170
Aurélien Questel <aurelien.questel@planisense.com>
2026-04-17 12:46:48 +0200

    roll back gh

--- src/lib/src/nael_utils/exception/exception.hh
+++ src/lib/src/nael_utils/exception/exception.hh
@@ -83,8 +83,6 @@ MAKE_EXCEPTION(fatal_error)
  * @param message the error message to throw in case of failure
  */
 #define CHECK_PRE(cond, message) \
-        do { \
-            if (!(cond)) { \
-                throw fatal_error(message); \
-            } \
-        } while (0)
+    if (!(cond)) { \
+        throw fatal_error(message); \
+    }
