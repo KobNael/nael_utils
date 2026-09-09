f50726c4725e29562ee13f5d031a54435aa19623
Aurélien Questel <aurelien.questel@planisense.com>
2026-09-09 09:31:07 +0200

    [skip ci] update doc

--- src/lib/src/nael_utils/computation/lcm.hh
+++ src/lib/src/nael_utils/computation/lcm.hh
@@ -10,6 +10,7 @@
  * @param a the first value
  * @param b the second value
  * @return the lowest common multiple of a and b
+ * @pre a != 0 && b != 0
  */
 unsigned long compute_lcm(unsigned long a, unsigned long b);
 
@@ -17,5 +18,7 @@ unsigned long compute_lcm(unsigned long a, unsigned long b);
  * @brief Compute the lowest common multiple of a list of integer values
  * @param values the list of values
  * @return the lowest common multiple of a list of integer values
+ * @pre values is not empty
+ * @pre values does not contain 0
  */
 unsigned long compute_lcm(const std::vector<unsigned>& values);
